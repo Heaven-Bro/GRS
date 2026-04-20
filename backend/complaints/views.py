1edf1e4bad7ce8ef9a56f655d3ac5f6d325d91c5
Heaven-Bro <heavenchakma04@gmail.com>
2026-04-20 15:12:10 +0600

    end backend

--- backend/complaints/views.py
+++ backend/complaints/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -19,12 +20,13 @@ def submit_complaint(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+
 @api_view(['GET'])
+@permission_classes([IsAdminUser])
 def get_all_complaints(request):
     complaints = Complaint.objects.all()
     serializer = ComplaintSerializer(complaints, many=True)
     return Response(serializer.data)
-
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
